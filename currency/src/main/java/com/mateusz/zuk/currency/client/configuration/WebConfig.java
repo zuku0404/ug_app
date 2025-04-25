0f31d5d177691a264ad99a98076a7d3fedc6d2ab
Mateusz Żuk <mzuk0493@gmail.com>
2025-04-25 16:28:21 +0200

    add Readme

--- currency/src/main/java/com/mateusz/zuk/currency/client/configuration/WebConfig.java
+++ currency/src/main/java/com/mateusz/zuk/currency/client/configuration/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500","http://127.0.0.1:3000")
+                .allowedOrigins("http://127.0.0.1:5500","http://127.0.0.1:3000","http://127.0.0.1:8081")
                 .allowedHeaders("*");
     }
 }
